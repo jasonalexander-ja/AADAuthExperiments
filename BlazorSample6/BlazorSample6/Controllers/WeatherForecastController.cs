f8cb2423d47e0a64cafcbea154be819096079e71
jasonalexander <61407072+jasonalexander-ja@users.noreply.github.com>
2022-02-23 16:50:10 +0000

    Initial commit

---
+++ BlazorSample6/BlazorSample6/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using BlazorSample.Data;
+
+namespace BlazorSample.Controllers
+{
+    [Authorize("App")]
+    [Route("Weather")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly WeatherForecastService weatherForecastService;
+
+        public WeatherForecastController(WeatherForecastService service)
+        {
+            weatherForecastService = service;
+        }
+        [HttpGet]
+        public async Task<WeatherForecast[]> GetWeatherForecast()
+        {
+            return await weatherForecastService.GetForecastAsync(System.DateTime.Now);
+        }
+    }
+}
